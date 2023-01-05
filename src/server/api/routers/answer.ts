7620962190bf407a504a31a198b7f034773a38e5
Gyozadaigaku <gaspardcube15@gmail.com>
2023-01-06 02:24:14 +0900

    ✨ add answer procedure

---
+++ src/server/api/routers/answer.ts
@@ -0,0 +1,102 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+
+export const answerRouter = createTRPCRouter({
+  postAnswer: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        answerContent: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.answer.create({
+          data: {
+            name: input.name,
+            userId: ctx.session.user.id,
+            answerContent: input.answerContent,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    try {
+      return await ctx.prisma.answer.findMany({
+        select: {
+          id: true,
+          name: true,
+          answerContent: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  }),
+  getOne: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        return await ctx.prisma.answer.findUnique({
+          where: {
+            id: input.id,
+          },
+          select: {
+            id: true,
+            name: true,
+            answerContent: true,
+            user: true,
+          },
+        });
+      } catch (error) {
+        console.log("error", error);
+      }
+    }),
+  deleteAnswer: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        return await ctx.prisma.answer.delete({
+          where: {
+            id: input.id,
+          },
+        });
+      } catch (error) {
+        console.log("error", error);
+      }
+    }),
+  updateAnswer: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        answerContent: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        return await ctx.prisma.answer.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            answerContent: input.answerContent,
+          },
+        });
+      } catch (error) {
+        console.log("error", error);
+      }
+    }),
+});
